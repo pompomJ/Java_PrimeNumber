d685ba9730f18c1d104ff6b168e4d5a019cd0557
pompomJ <67637461+pompomJ@users.noreply.github.com>
2024-09-28 12:03:37 +0900

    Add files via upload

---
+++ Java_PrimeNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("数字を入力してください: ");
+        int num = sc.nextInt();
+
+        boolean isPrime = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+	if (num == 1) {
+	   System.out.println(num + "は素数ではありません。");
+	}
+
+        else if (isPrime) {
+            System.out.println(num + "は素数です。");
+        } else {
+            System.out.println(num + "は素数ではありません。");
+        }
+    }
+}
